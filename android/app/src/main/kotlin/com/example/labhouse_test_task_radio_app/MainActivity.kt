b223247bd5fa46a774b8d75add778ec195cb8786
DmytroMeleshko <meleshkodim2000@gmail.com>
2024-01-20 12:54:45 +0100

    Create initial app

---
+++ android/app/src/main/kotlin/com/example/labhouse_test_task_radio_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.labhouse_test_task_radio_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RadioApp());
+}
+
+class RadioApp extends StatelessWidget {
+  const RadioApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Radio Stations App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
