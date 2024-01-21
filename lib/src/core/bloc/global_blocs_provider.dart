afe90ce0c466c9f40c8ebf82345427bfe83e9276
DmytroMeleshko <meleshkodim2000@gmail.com>
2024-01-21 23:35:24 +0100

    Add global blocs provider

---
+++ lib/src/core/bloc/global_blocs_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../config/di/injection.dart';
+import '../../modules/radio/blocs/favorite_stations_bloc/favorite_stations_bloc.dart';
+import '../../modules/radio/blocs/radio_stations_bloc/radio_stations_bloc.dart';
+
+class GlobalBlocsProvider extends StatelessWidget {
+  const GlobalBlocsProvider({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<FavoriteStationsBloc>(
+          create: (_) => getIt()..add(const FavoriteStationsEvent.loadAll()),
+        ),
+        BlocProvider<RadioStationsBloc>(
+          create: (_) => getIt()..add(const RadioStationsEvent.load()),
+        ),
+      ],
+      child: child,
+    );
+  }
+}
