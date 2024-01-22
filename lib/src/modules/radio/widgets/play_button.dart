e46d765f5537a646d4f05d9fb6911c2306d34867
DmytroMeleshko <meleshkodim2000@gmail.com>
2024-01-22 23:09:51 +0100

    Add PlayButton widget and loader

---
+++ lib/src/modules/radio/widgets/play_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class PlayButton extends StatelessWidget {
+  const PlayButton({
+    required this.isPlaying,
+    required this.isAudioLoading,
+    this.iconSize = 40,
+    this.onTap,
+    super.key,
+  });
+
+  final bool isPlaying;
+  final bool isAudioLoading;
+  final VoidCallback? onTap;
+  final double iconSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconButton(
+          icon: Icon(
+            isPlaying
+                ? Icons.pause_rounded
+                : Icons.play_arrow_rounded,
+            size: iconSize,
+          ),
+          onPressed: onTap,
+        ),
+
+        if (isAudioLoading)
+          Positioned.fill(
+            child: IgnorePointer(
+              ignoring: true,
+              child: Container(
+                padding: EdgeInsets.all(iconSize * 0.4),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background.withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
